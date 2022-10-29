e19e899f0d393cdba0f0d0f97c12648d9fe9377d
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2022-10-29 17:40:39 -0400

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int system(const char *command);
+
+__attribute__((constructor))
+static void customConstructor(int argc, const char **argv)
+{	
+	system("open -a Calculator.app");	
+}
